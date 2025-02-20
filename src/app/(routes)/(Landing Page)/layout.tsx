d45f169f62250c7107c8effc2a4985262e004a12
DevYemi <silasadegoke331@gmail.com>
2025-02-20 08:23:36 +0100

    fix:window listener for screen width in layout

--- src/app/(routes)/(Landing Page)/layout.tsx
+++ src/app/(routes)/(Landing Page)/layout.tsx
@@ -4,12 +4,17 @@ import Header from "./_local-components/Header";
 import Footer from "./_local-components/Footer";
 import ComingSection from "./_local-components/ComingSection";
 import { attachCloudinaryPrefix } from "@/app/hooks/attachCloudinaryPrefix";
-import { useMediaQuery } from "react-responsive";
+import { useEffect,useState } from "react";
 type Props = {
   children: React.ReactNode;
 };
 function LandingPagelayout({ children }: Props) {
-  const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+    useEffect(() => {
+      if (window.innerWidth <= 800) {
+        setIsMobile(true);
+      }
+    },[]);
 
   const backgroundImageUrl = attachCloudinaryPrefix(
     "v1739959653/shutterstock_1969544767_on7k1r.webp"
